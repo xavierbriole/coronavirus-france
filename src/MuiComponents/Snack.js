2de3a874ff25b934d09524393f3bf3f31add3f7c
xavierbriole <xbriole@icloud.com>
2020-10-29 22:18:46 +0100

    Add new attests

--- src/MuiComponents/Snack.js
+++ src/MuiComponents/Snack.js
@@ -2,8 +2,7 @@
 
 import * as React from 'react'
 import Snackbar from '@material-ui/core/Snackbar'
-import IconButton from '@material-ui/core/IconButton'
-import CloseIcon from '@material-ui/icons/Close'
+import MuiAlert from '@material-ui/lab/Alert'
 
 type Props = {|
   message: string,
@@ -28,20 +27,18 @@ export default function PositionedSnackbar({
     <Snackbar
       anchorOrigin={{ vertical, horizontal }}
       open={open}
-      message={message}
       key={vertical + horizontal}
       autoHideDuration={20000}
       style={{ maxWidth: 300 }}
-      action={
-        <IconButton
-          size='small'
-          aria-label='close'
-          color='inherit'
-          onClick={handleClose}
-        >
-          <CloseIcon fontSize='small' />
-        </IconButton>
-      }
-    />
+    >
+      <MuiAlert
+        elevation={6}
+        variant='filled'
+        onClose={handleClose}
+        severity='error'
+      >
+        {message}
+      </MuiAlert>
+    </Snackbar>
   )
 }
